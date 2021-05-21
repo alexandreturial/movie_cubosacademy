a319ee5834a9ff36c1c265b4a378d29b2f9c31cf
xande <aleturial8@gmail.com>
2021-05-21 15:34:43 -0300

    list by genre test screen

---
+++ test/list_by_genre_test.dart
@@ -0,0 +1,33 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:movie_desafio/API/api.dart';
+import 'package:movie_desafio/Screen_By_Genres.dart/models/model.dart';
+import 'package:movie_desafio/core/JsonType/movies.dart';
+
+
+void main() {
+
+  test('fetchGenres and fetchMovie should fill movies and genres variable',(){
+    final model = ListMoviesModel(api: MockAPI());
+
+    model.fetchMoveis(15);
+
+     model.movies.then((movies){
+       expect(movies.movies[0].title, 'title');
+     });
+
+  });
+}
+
+class MockAPI extends API{
+  
+  @override
+  Future<Movies> fetchMovie(){
+    final movie = Movie(0,'title','img');
+    final movies = Movies([movie]);
+
+    return Future.value(movies);
+  }
+
+}
+
+
